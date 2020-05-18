def4633d2beeb002c8404109d76713be0249c3fa
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-19 00:00:32 +0530

    :tada: Initialize @staart/ui app

---
+++ plugins/axios.ts
@@ -0,0 +1,103 @@
+import Vue from "vue";
+import { NuxtAxiosInstance } from "@nuxtjs/axios";
+import { AxiosRequestConfig } from "axios";
+import decode from "jwt-decode";
+import { ToastProgrammatic as Toast } from "buefy";
+
+const ignoredErrors = ["no-customer"];
+
+export default function({
+  $axios,
+  redirect,
+  store,
+  app,
+}: {
+  $axios: NuxtAxiosInstance;
+  app: any;
+  redirect: any;
+  store: {
+    state: {
+      auth: any;
+    };
+    dispatch: any;
+  };
+}) {
+  $axios.interceptors.request.use(
+    (config: AxiosRequestConfig) =>
+      new Promise((resolve, reject) => {
+        console.log("Axios request starting");
+        $axios.setHeader("X-Requested-With", "XMLHttpRequest");
+        if (process.env.API_KEY)
+          $axios.setHeader("X-Api-Key", process.env.API_KEY);
+
+        try {
+          const token: string = config.headers.common.Authorization.replace(
+            "Bearer ",
+            ""
+          );
+          if (
+            decode<{ exp: number }>(token).exp * 1000 <
+            new Date().getTime()
+          ) {
+            console.log("Axios token is expired");
+            if (!store.state.auth.tokens.refresh) {
+              console.log("Could not get refresh token in store");
+              return resolve(config);
+            }
+            console.log("Refreshing token");
+            $axios.setHeader("Authorization", undefined);
+            store
+              .dispatch("auth/refresh")
+              .then((newToken: string) => {
+                console.log("Got new token", newToken);
+                $axios.setHeader("Authorization", newToken);
+                config.headers = {
+                  ...config.headers,
+                  Authorization: `Bearer ${newToken}`,
+                };
+              })
+              .catch(() => store.dispatch("auth/logout"))
+              .then(() => resolve(config));
+          } else {
+            resolve(config);
+          }
+        } catch (error) {
+          resolve(config);
+        }
+      })
+  );
+  $axios.onResponse((response) => {
+    if (typeof response.data?.text === "string") {
+      Toast.open({
+        message: response?.data?.text,
+        type: "is-success",
+      });
+    }
+  });
+  $axios.onError((error) => {
+    if (!error.response) return;
+
+    if (
+      [
+        "revoked-token",
+        "invalid-token",
+        "expired-token",
+        "missing-token",
+      ].includes(error.response.data.code || error.response.data.error)
+    ) {
+      return redirect("/auth/refresh");
+    }
+
+    if (
+      ignoredErrors.includes(
+        error.response.data.code || error.response.data.error
+      )
+    ) {
+    } else if (typeof error.response.data?.error === "string") {
+      Toast.open({
+        message: error?.response?.data?.error,
+        type: "is-danger",
+      });
+    }
+  });
+}
