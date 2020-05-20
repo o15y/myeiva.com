56be0b5d3050191bfbbae116ef31c5ab0af02f7a
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-20 18:33:17 +0530

    :sparkles: Location page

--- pages/teams/_username/locations.vue
+++ pages/teams/_username/locations.vue
@@ -1,38 +1,26 @@
 <template>
   <div>
-    <h1 class="is-size-4">Team members</h1>
+    <h1 class="is-size-4">Locations</h1>
     <b-table
       :loading="loading"
-      :data="members.data"
+      :data="location.data"
       default-sort-direction="asc"
       sort-icon="arrow-up"
       sort-icon-size="is-small"
     >
       <template slot-scope="props">
-        <b-table-column sortable field="member" label="Name">
-          {{ props.row.user.name }}
+        <b-table-column sortable field="type" label="Type">
+          <span>{{ locationTypeLabel(props.row.type) }}</span>
         </b-table-column>
-        <b-table-column sortable field="role" label="Role">
-          <b-select
-            :value="props.row.role"
-            @input="(val) => updateRole(props.row.id, val)"
-            :loading="loadingId === props.row.id"
-            placeholder="Update role"
-          >
-            <option value="OWNER">Owner</option>
-            <option value="ADMIN">Admin</option>
-            <option value="MEMBER">Member</option>
-          </b-select>
-        </b-table-column>
-        <b-table-column sortable field="createdAt" label="Added">
-          {{ new Date(props.row.createdAt).toLocaleDateString() }}
+        <b-table-column sortable field="value" label="Location">
+          <span>{{ truncate(props.row.value) }}</span>
         </b-table-column>
         <b-table-column class="has-text-right">
           <b-tooltip label="Remove">
             <b-button
               type="is-danger"
               icon-right="delete"
-              @click="deleteMember(props.row.id, props.row.user.name)"
+              @click="deleteLocation(props.row.id)"
             />
           </b-tooltip>
         </b-table-column>
@@ -40,39 +28,85 @@
     </b-table>
     <div class="has-text-centered">
       <b-button
-        v-if="members.hasMore"
+        v-if="location.hasMore"
         @click="get"
         icon-right="arrow-down"
         :loading="loading"
       >
-        Load more members
+        Load more location
       </b-button>
     </div>
-    <h2 class="is-size-5" style="margin-top: 1rem">Add member</h2>
+    <h2 class="is-size-5" style="margin-top: 1rem">Add location</h2>
     <form @submit.prevent="add" style="margin-top: 0.5rem">
+      <b-field label="Type">
+        <div class="block">
+          <b-radio
+            v-model="newLocationType"
+            name="newLocationType"
+            native-value="VIDEO_CALL"
+          >
+            {{ locationTypeLabel("VIDEO_CALL") }}
+          </b-radio>
+          <b-radio
+            v-model="newLocationType"
+            name="newLocationType"
+            native-value="PHONE_CALL"
+          >
+            {{ locationTypeLabel("PHONE_CALL") }}
+          </b-radio>
+          <b-radio
+            v-model="newLocationType"
+            name="newLocationType"
+            native-value="IN_PERSON"
+          >
+            {{ locationTypeLabel("IN_PERSON") }}
+          </b-radio>
+        </div>
+      </b-field>
       <div class="columns">
-        <div class="column">
-          <b-field label="Name">
-            <b-input type="text" v-model="newMemberName" required />
-          </b-field>
-        </div>
-        <div class="column">
-          <b-field label="Email">
-            <b-input type="email" v-model="newMemberEmail" required />
-          </b-field>
-        </div>
-        <div class="column">
-          <b-field label="Role">
-            <b-select v-model="newMemberRole" expanded>
-              <option value="OWNER">Owner</option>
-              <option value="ADMIN">Admin</option>
-              <option value="MEMBER">Member</option>
+        <div class="column" v-if="newLocationType === 'VIDEO_CALL'">
+          <b-field label="Service">
+            <b-select
+              placeholder="Select a service"
+              v-model="newLocationService"
+              expanded
+            >
+              <option>Jitsi Meet</option>
+              <option>Zoom</option>
+              <option>Google Meet</option>
+              <option>Skype</option>
+              <option>Whereby</option>
+              <option>Messenger</option>
+              <option>Telegram</option>
+              <option>WhatsApp</option>
+              <option>Viber</option>
+              <option>Houseparty</option>
+              <option>Signal</option>
+              <option>LINE</option>
+              <option>WeChat</option>
+              <option>Other</option>
             </b-select>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-field :label="newLocationLabel">
+            <b-input
+              :icon="
+                newLocationType === 'IN_PERSON'
+                  ? 'map'
+                  : newLocationType === 'VIDEO_CALL'
+                  ? 'link'
+                  : 'phone'
+              "
+              type="text"
+              v-model="newLocationValue"
+              required
+            />
           </b-field>
         </div>
       </div>
       <b-button type="is-primary" native-type="submit" :loading="loadingAdd">
-        Invite member
+        Invite location
       </b-button>
     </form>
   </div>
@@ -81,7 +115,7 @@
 <script lang="ts">
   import { mapGetters } from "vuex";
   import { Vue, Component, Watch } from "vue-property-decorator";
-  import download from "js-file-download";
+  import { truncate } from "../../../util/string";
 
   @Component({
     middleware: "authenticated",
@@ -90,48 +124,30 @@
   export default class UsersProfile extends Vue {
     loading = false;
     loadingAdd = false;
-    loadingId = 0;
-    team: any = {};
-    members: any = { data: [] };
-    newMemberName = "";
-    newMemberEmail = "";
-    newMemberRole = "MEMBER";
+    location: any = { data: [] };
+    truncate = truncate;
+
+    newLocationValue = "";
+    newLocationType = "VIDEO_CALL";
+    newLocationService = "Jitsi Meet";
 
     async created() {
       return this.get();
-    }
-
-    async updateRole(id: number, role: string) {
-      this.loadingId = id;
-      try {
-        const { data } = await this.$axios.patch(
-          `/organizations/${this.$route.params.username}/memberships/${id}`,
-          {
-            role,
-          }
-        );
-      } catch (error) {
-        this.members.data = [];
-        this.get();
-      }
-      this.loadingId = 0;
     }
 
     async get() {
       this.loading = true;
       try {
         const { data } = await this.$axios.get(
-          `/organizations/${
-            this.$route.params.username
-          }/memberships?first=10&include=user${
-            this.members.data.length
-              ? `&after=${this.members.data[this.members.data.length - 1].id}`
+          `/organizations/${this.$route.params.username}/locations?first=10${
+            this.location.data.length
+              ? `&after=${this.location.data[this.location.data.length - 1].id}`
               : ""
           }`
         );
-        this.members.data.push(...(data.data || []));
-        this.members.hasMore = data.hasMore;
-        this.members = data;
+        this.location.data.push(...(data.data || []));
+        this.location.hasMore = data.hasMore;
+        this.location = data;
       } catch (error) {}
       this.loading = false;
     }
@@ -140,26 +156,31 @@
       this.loadingAdd = true;
       try {
         const { data } = await this.$axios.put(
-          `/organizations/${this.$route.params.username}/memberships`,
+          `/organizations/${this.$route.params.username}/locations`,
           {
-            name: this.newMemberName,
-            email: this.newMemberEmail,
-            role: this.newMemberRole,
+            value: this.newLocationValue,
+            type: this.newLocationType,
+            data:
+              this.newLocationService && this.newLocationService !== "Other"
+                ? JSON.stringify({
+                    name: this.newLocationService,
+                    template: "{{name}}: {{value}}",
+                  })
+                : undefined,
           }
         );
-        this.members.data.push(data.added);
-        this.newMemberName = "";
-        this.newMemberEmail = "";
-        this.newMemberRole = "";
+        this.location.data.push(data.added);
+        this.newLocationValue = "";
+        this.newLocationType = "";
       } catch (error) {}
       this.loadingAdd = false;
     }
 
-    async deleteMember(id: number, member: string) {
+    async deleteLocation(id: number) {
       this.$buefy.dialog.confirm({
-        title: "Removing member",
-        message: `Are you sure you want to remove <strong>${member}</strong> from this team? This action is not reversible, and you'll have to add them again if you change your mind.`,
-        confirmText: "Yes, remove member",
+        title: "Removing location",
+        message: `Are you sure you want to remove this location? This action is not reversible, and you'll have to add it again if you change your mind.`,
+        confirmText: "Yes, remove location",
         cancelText: "No, don't remove",
         type: "is-danger",
         hasIcon: true,
@@ -168,7 +189,7 @@
           this.loading = true;
           try {
             await this.$axios.delete(
-              `/organizations/${this.$route.params.username}/memberships/${id}`
+              `/organizations/${this.$route.params.username}/locations/${id}`
             );
           } catch (error) {}
           return this.get();
@@ -176,18 +197,18 @@
       });
     }
 
-    async verifyMember(method: string, id: number, member: string) {
-      this.loading = true;
-      try {
-        const { data } = await this.$axios.post(
-          `/organizations/${this.$route.params.username}/memberships/${id}/verify`,
-          {
-            method,
-          }
-        );
-        this.team = data.updated;
-      } catch (error) {}
-      this.loading = false;
+    locationTypeLabel(key: string) {
+      if (key === "IN_PERSON") return "In-person meeting";
+      if (key === "PHONE_CALL") return "Phone call";
+      if (key === "VIDEO_CALL") return "Video call";
+      return key;
+    }
+
+    get newLocationLabel() {
+      if (this.newLocationType === "IN_PERSON") return "Address";
+      if (this.newLocationType === "PHONE_CALL") return "Phone number";
+      if (this.newLocationType === "VIDEO_CALL") return "URL or details";
+      return "Details";
     }
   }
 </script>
