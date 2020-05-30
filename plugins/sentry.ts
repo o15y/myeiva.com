0d6d9ebdc77424f6ada68594c60e0be77d224997
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-30 22:48:34 +0530

    :recycle: Only track errors on client

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -1,8 +1,10 @@
-import { init, captureMessage } from "@sentry/browser";
+import { init } from "@sentry/browser";
 
 export default () => {
-  init({
-    dsn:
-      "https://957d1a6c8ebc443f9bf3136dcc77a948@o400462.ingest.sentry.io/5258918",
-  });
+  if (process.client) {
+    init({
+      dsn:
+        "https://957d1a6c8ebc443f9bf3136dcc77a948@o400462.ingest.sentry.io/5258918",
+    });
+  }
 };
